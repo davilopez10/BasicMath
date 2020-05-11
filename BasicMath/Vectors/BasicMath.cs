9eda80ee6ea761dc0f705929bad37f1646c8e711
davilopez10 <50497090+davilopez10@users.noreply.github.com>
2020-05-11 16:16:02 +0100

    Add files via upload

---
+++ BasicMath/Vectors/BasicMath.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public static class BasicMath
+{
+    public const float Rad2Deg = 57.295779513082320876798154814105f;
+
+    public static float Magnitude(Vector2 vector)
+    {
+        return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y);
+    }
+
+    public static float SqrMagnitude(Vector2 vector)
+    {
+        return vector.x * vector.x + vector.y * vector.y;
+    }
+
+    public static Vector2 Normalize(Vector2 vector)
+    {
+        return vector / Magnitude(vector);
+    }
+
+    public static Vector2 Vector2Lerp(Vector2 a, Vector2 b, float t)
+    {
+        t = Mathf.Clamp01(t);
+        return a + t * (b - a);
+    }
+
+    public static float Dot(Vector2 a, Vector2 b)
+    {
+        return a.x * b.x + a.y * b.y;
+    }
+
+    public static float AngleDeg(Vector2 a, Vector2 b)
+    {
+        return AngleRad(Normalize(a), Normalize(b)) * Rad2Deg;
+    }
+
+    public static float AngleRad(Vector2 a, Vector2 b)
+    {
+        return Mathf.Acos(Dot(Normalize(a), Normalize(b)));
+    }
+}
---
+++ BasicMath/Vectors/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 2f;
+
+    [Range(0, 1)]
+    public float t;
+
+    private Vector3 initPos;
+
+    private void MoveToTarget()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        transform.position += (Vector3)BasicMath.Normalize(dir) * speed * Time.deltaTime;
+    }
+
+    private void MoveToTargetLerp()
+    {
+        transform.position = BasicMath.Vector2Lerp(initPos, target.position, t);
+    }
+
+    private void Awake()
+    {
+        initPos = transform.position;
+    }
+
+    private void Dot()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        Vector3 normalizedA = BasicMath.Normalize(transform.up);
+        Vector3 normalizedB = BasicMath.Normalize(dir);
+
+        Debug.Log(BasicMath.Dot(normalizedA, normalizedB));
+    }
+
+    private void Angle()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        Debug.Log(BasicMath.AngleDeg(transform.up, dir));
+    }
+
+    private void Update()
+    {
+        //MoveToTargetLerp();
+        //MoveToTarget();
+        //Dot();
+        Angle();
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * 2);
+        Vector3 dir = target.position - transform.position;
+        Gizmos.DrawLine(transform.position, transform.position + dir);
+    }
+}
